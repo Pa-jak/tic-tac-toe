a10267e8142954ec0f7d0c2b123f2ebecf5e6f6c
Michal Pajak <michalpajak96@gmail.com>
2022-11-19 00:57:17 +0000

    easy AI added

--- main.js
+++ main.js
@@ -47,11 +47,13 @@ const winConditions = () =>{
     if(score[0] === 'X' && score[1] === 'X' && score[2] === 'X' || score[3] === 'X' && score[4] === 'X' && score[5] === 'X' || score[6] === 'X' && score[7] === 'X' && score[8] === 'X' || score[0] === 'X' && score[3] === 'X' && score[6] === 'X' || score[1] === 'X' && score[4] === 'X' && score[7] === 'X' || score[2] === 'X' && score[5] === 'X' && score[8] === 'X' || score[0] === 'X' && score[4] === 'X' && score[8] === 'X' || score[2] === 'X' && score[4] === 'X' && score[6] === 'X'){
         playerXwin()
         win()
+        
     }
 //win O
     else if(score[0] === 'O' && score[1] === 'O' && score[2] === 'O' || score[3] === 'O' && score[4] === 'O' && score[5] === 'O' || score[6] === 'O' && score[7] === 'O' && score[8] === 'O' || score[0] === 'O' && score[3] === 'O' && score[6] === 'O' || score[1] === 'O' && score[4] === 'O' && score[7] === 'O' || score[2] === 'O' && score[5] === 'O' && score[8] === 'O' || score[0] === 'O' && score[4] === 'O' && score[8] === 'O' || score[2] === 'O' && score[4] === 'O' && score[6] === 'O'){
         playerOwin()
         win()
+        
     }       
 //drow
     else if(playerTurn == 9){
@@ -90,12 +92,38 @@ for(let i = 0; i < divs.length ; i++){
     divs[i].addEventListener('click', ()=>{
 // player turn
         if(spans[i].textContent == ''){
-            if(playerTurn%2 == 0){
+            if(playerTurn%2 == 0 ){
                 char = 'X'
                 score[i] = 'X'
                 scoreO.style.border = '2px solid red';
                 scoreX.style.border = '2px solid black';
-            }else if (playerTurn%2 != 0){
+
+                if(playerO.isAi ){
+    
+                    const losu = ()=>{
+                    if(spans[random].textContent == '' && playerTurn != 0){
+                        console.log('mamy O', random, winConditions() == true);
+                        
+                        spans[random].textContent = 'O'
+                        score[random] = 'O'
+                        scoreX.style.border = '2px solid red';
+                        scoreO.style.border = '2px solid black';
+                        
+                        playerTurn ++; 
+                        random = Math.floor(Math.random() * divs.length);
+                        winConditions()
+                        
+
+                    }else if(playerTurn != 0){
+                        console.log(random, 'z elsa')
+                        random = Math.floor(Math.random() * divs.length) ;
+                        losu()
+                    }
+                    }
+    // losu()
+            setTimeout(losu, 1000)
+                }
+            }else if (playerTurn%2 != 0 && playerO.isAi === 0){
                 char = 'O'
                 score[i] = 'O'
                 scoreX.style.border = '2px solid red';
